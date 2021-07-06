802a530c593c7767e6363811b48f774fbb738465
Bichi Kim <bichi@live.co.kr>
2021-07-06 16:56:37 +0900

    update

--- src/pages/Main.ts
+++ src/pages/Main.ts
@@ -6,13 +6,23 @@ export const Main = defineComponent({
   setup() {
     const valueRef = ref('')
     const inputRef = ref('')
-    const {write} = useAppClipboard(valueRef)
+    const {write, read, state} = useAppClipboard(valueRef)
+
+    const readAndUpdateInput = async () => {
+      const value = await read()
+      if (value) {
+        inputRef.value = value
+      }
+    }
+
     return () => (
       h('div', {style: {paddingTop: '100px'}}, [
+        h('div', state.value),
         h('div', valueRef.value),
         h('div', inputRef.value),
         h('input', {onInput: (event) => (inputRef.value = event.target.value), value: inputRef.value}),
-        h('button', {onClick: () => write(inputRef.value)}, 'copy'),
+        h('button', {onClick: () => write(inputRef.value)}, 'write'),
+        h('button', {onClick: () => readAndUpdateInput()}, 'read'),
       ])
     )
   },
