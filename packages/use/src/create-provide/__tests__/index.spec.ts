ef56c6de18be393f1d2709c4ffa51d4136cae24f
Bichi Kim <bichi@live.co.kr>
2021-07-22 01:29:23 +0900

    update

---
+++ packages/use/src/create-provide/__tests__/index.spec.ts
@@ -0,0 +1,44 @@
+import {createProvide} from '../'
+import {defineComponent, h} from 'vue'
+import {mount} from '@vue/test-utils'
+import {expectType} from 'tsd'
+
+const setup = () => {
+
+  const {provide, inject} = createProvide('foo')
+
+  const Component2 = defineComponent({
+    setup() {
+      const value = inject()
+
+      expectType<string | undefined>(value)
+
+      return () => (
+        h('div', value)
+      )
+    },
+  })
+
+  const Component = defineComponent({
+    setup() {
+      provide()
+      return () => (
+        h(Component2)
+      )
+    },
+  })
+
+  const wrapper = mount(Component)
+
+  return {
+    wrapper,
+  }
+}
+
+describe('createProvide', () => {
+  it('should provide & inject data', () => {
+    const {wrapper} = setup()
+
+    expect(wrapper.text()).toBe('foo')
+  })
+})
---
+++ packages/use/src/create-provide/index.ts
@@ -0,0 +1,19 @@
+import * as process from 'process'
+import {inject, InjectionKey, provide} from 'vue'
+import {freeze, MayFunction, mayFunctionValue, UnFunction} from '@winter-love/utils'
+
+export const createProvide = <T extends MayFunction<unknown>>(data: T, name?: string) => {
+
+  const key: InjectionKey<UnFunction<T>> = Symbol(process.env.NODE_ENV === 'development' ? name : undefined)
+
+  return freeze({
+    inject: () => {
+      return inject<UnFunction<T>>(key)
+    },
+    injectKey: key,
+    provide: () => {
+      const _data = mayFunctionValue(data)
+      return provide(key, _data)
+    },
+  })
+}
