0f6ce1bee92de184a08dcdbb5287585c323c528a
Bichi Kim <bichi@live.co.kr>
2021-07-27 12:45:05 +0900

    update

--- packages/use/src/route-history/index.ts
+++ packages/use/src/route-history/index.ts
@@ -1,3 +1,5 @@
+import {ref, watch} from 'vue'
+
 export interface RouteHistory {
   back: string | null
   current: string
@@ -7,19 +9,28 @@ export interface RouteHistory {
   scroll: {left: number; top: number}
 }
 
+const original: any = window.onpopstate
+const stateRef = ref<RouteHistory>()
+const locationRef = ref<Location>(document.location)
+
+function onPopState(this: WindowEventHandlers, event: PopStateEvent) {
+  if (typeof original === 'function') {
+    original(event)
+  }
+  stateRef.value = event.state
+  locationRef.value = document.location
+}
+
+window.onpopstate = onPopState
+
 /**
  * WIP
  * @param callback
  */
 export const onRouteHistory = (callback: (state: RouteHistory, location: Location) => unknown) => {
-  const original: any = window.onpopstate
-
-  function onPopState(this: WindowEventHandlers, event: PopStateEvent) {
-    if (typeof original === 'function') {
-      original(event)
+  watch([stateRef, locationRef], ([state, location]) => {
+    if (state) {
+      callback(state, location)
     }
-    callback(event.state, document.location)
-  }
-
-  window.onpopstate = onPopState
+  })
 }
