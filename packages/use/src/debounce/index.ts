53a8ee32ac2cd57ee8c44da793dd26c1ef9dd511
Bichi Kim <bichi@live.co.kr>
2021-07-30 13:40:51 +0900

    update

--- packages/use/src/debounce/index.ts
+++ packages/use/src/debounce/index.ts
@@ -3,25 +3,25 @@ import {computed, onBeforeUnmount} from 'vue-demi'
 import {wrapRef} from 'src/wrap-ref'
 import {MayRef} from 'src/types'
 
-export type UseDebounceHandle = () => unknown
+export type UseDebounceHandle<Args extends any[]> = (...args: Args) => unknown
 
 const defaultWait = 150
 
-export const useDebounce = (
-  handle?: UseDebounceHandle,
+export const useDebounce = <Args extends any[]>(
+  handle?: UseDebounceHandle<Args>,
   wait: MayRef<number> = defaultWait,
   immediate: boolean = false,
 ) => {
   const waitRef = wrapRef(wait)
   const onCall = computed(() => {
-    return debounce(() => handle?.(), waitRef.value, immediate)
+    return debounce((...args: Args) => handle?.(...args), waitRef.value, immediate)
   })
 
   onBeforeUnmount(() => {
     onCall.value.clear()
   })
 
-  return () => {
-    onCall.value()
+  return (...args: Args) => {
+    onCall.value(...args)
   }
 }
