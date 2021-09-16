7aee513e13c2f37493096b3c32a5be6085bfcc42
bichikim <bichi@live.co.kr>
2021-09-17 00:07:02 +0900

    update stitches

--- packages/stitches/src/creare-styled.ts
+++ packages/stitches/src/creare-styled.ts
@@ -4,29 +4,28 @@ import {createDirective} from './directive'
 
 export interface StyledOptions {
   name?: string
-  stylePortal?: string
 }
 
 export const createStyled = <S extends Stitches>(stitches: S) => {
   return function styled(element, options: StyledOptions = {}, ...systems: Parameters<S['css']>) {
-    const {name, stylePortal = 'css'} = options
+    const {name} = options
     const directive = createDirective(stitches as any, ...systems as any)
     return defineComponent({
       name,
       props: {
         as: {type: String},
-        [stylePortal]: {default: () => ({}), type: Object},
+        css: {default: () => ({}), type: Object},
+        variants: {default: () => ({}), type: Object},
       },
       setup(props) {
         const asRef = toRef(props, 'as')
-        const cssRef = toRef(props, stylePortal)
 
         const elementRef = computed(() => {
           return asRef.value ?? element
         })
 
         return () => (
-          withDirectives(h(elementRef.value), [[directive, cssRef.value]])
+          withDirectives(h(elementRef.value), [[directive, [props.css, props.variants]]])
         )
       },
     })
--- packages/stitches/src/directive.ts
+++ packages/stitches/src/directive.ts
@@ -1,62 +1,93 @@
 
 import {DirectiveBinding, ObjectDirective} from 'vue-demi'
 import Stitches from '@stitches/core/types/stitches'
+import {CssComponent} from '@stitches/core/types/styled-component'
 
 export interface StitchesInfo {
   previousClassNames?: string[]
 }
+
+export type DirectiveBindingValue = 
+  Record<string, any> |
+  [Record<string, any>] |
+  [Record<string, any>, Record<string, any>]
 
 export interface CreateDirectiveOptions {
   systems?: Record<string, (props: any) => unknown>
   theme?: Record<string, any>
 }
 
-export type EmotionElement = Element & {
+export type StitchesElement = Element & {
   __stitches__?: StitchesInfo
 }
 
-export const createDirective = <S extends Stitches>(stitches: S, ...systems: Parameters<S['css']>): ObjectDirective => {
+
+export const getCssList = (
+  system: CssComponent,
+  css: Record<string, any>,
+  variants: Record<string, any> = {},
+) => {
+  return system({...variants, css}).className.split(' ')
+}
+
+export const getClassName = (
+  system: CssComponent,
+  binding: DirectiveBinding<any>,
+) => {
+  const {value} = binding
+
+  if (typeof value !== 'object') {
+    return
+  }
+
+  if (Array.isArray(value)) {
+    const [css, variants] = value
+    return getCssList(system, css, variants)
+  }
+
+  return getCssList(system, value)
+}
+
+const updateClassName = (
+  system: CssComponent,
+  el: StitchesElement,
+  binding: DirectiveBinding<DirectiveBindingValue>,
+) => {
+  const {previousClassNames} = el.__stitches__ ?? {}
+  if (previousClassNames) {
+    el.classList.remove(...previousClassNames)
+  }
+
+  const classNames = getClassName(system, binding)
+
+  el.__stitches__ = {
+    previousClassNames: classNames,
+  }
+
+  if (classNames) {
+    el.classList.add(...classNames)
+  }
+}
+
+export const createDirective = <S extends Stitches>(
+  stitches: S,
+  ...systems: Parameters<S['css']>
+): ObjectDirective<StitchesElement, DirectiveBindingValue> => {
   const {css} = stitches
 
   const system = css(...systems as any)
 
-  const getClassName = (binding: DirectiveBinding<any>) => {
-    const {value} = binding
-
-    if (typeof value !== 'object' || Array.isArray(value)) {
-      return
-    }
-    return system({css: value}).className.split(' ')
-  }
-
-  const updateClassName = (el: EmotionElement, binding: DirectiveBinding<any>) => {
-    const {previousClassNames} = el.__stitches__ ?? {}
-    if (previousClassNames) {
-      el.classList.remove(...previousClassNames)
-    }
-
-    const classNames = getClassName(binding)
-
-    el.__stitches__ = {
-      previousClassNames: classNames,
-    }
-
-    if (classNames) {
-      el.classList.add(...classNames)
-    }
-  }
-
   return {
     getSSRProps(binding) {
       return {
-        class: getClassName(binding),
+        class: getClassName(system, binding),
       }
     },
-    mounted(el: EmotionElement, binding) {
-      updateClassName(el, binding)
+    mounted(el: StitchesElement, binding) {
+      updateClassName(system, el, binding)
     },
-    updated(el: EmotionElement, binding) {
-      updateClassName(el, binding)
+    updated(el: StitchesElement, binding) {
+      updateClassName(system, el, binding)
     },
   }
 }
