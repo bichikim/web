1d3780fdc5d8c9dac2696a6a08c8329d42d07962
Bichi Kim <bichi@live.co.kr>
2021-10-10 19:47:07 +0900

    update

--- coong/client/capacitor.config.ts
+++ coong/client/capacitor.config.ts
@@ -1,15 +1,9 @@
 import {CapacitorConfig} from '@capacitor/cli'
 
-console.log('foo')
-
 const config: CapacitorConfig = {
   appId: 'io.coong.app',
   appName: 'coong',
   bundledWebRuntime: false,
-  server: {
-    cleartext: true,
-    url: 'http://192.168.35.123:3000',
-  },
   webDir: 'dist/spa',
 }
 
--- coong/client/vite.config.ts
+++ coong/client/vite.config.ts
@@ -4,9 +4,11 @@ import VueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
 import {Quasar} from 'quasar'
 import {defineConfig} from 'vite'
-import ViteComponents from 'vite-plugin-components'
-import ViteIcons, {ViteIconsResolver} from 'vite-plugin-icons'
+import Components from 'unplugin-vue-components/vite'
+import {QuasarResolver} from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
 // import Markdown from 'vite-plugin-md'
+import IconsResolver from 'unplugin-icons/resolver'
 import {VitePWA} from 'vite-plugin-pwa'
 import ssr from 'vite-ssr-vue/plugin'
 // import Prism from 'markdown-it-prism'
@@ -68,28 +70,35 @@ export default defineConfig({
     // }),
 
     // https://github.com/antfu/vite-plugin-components
-    ViteComponents({
-
-      // auto import icons
-      customComponentResolvers: [
-        // https://github.com/antfu/vite-plugin-icons
-        ViteIconsResolver({
-          componentPrefix: '',
-          enabledCollections: ['carbon'],
-        }),
+    Components({
+      resolvers: [
+        QuasarResolver(),
+        IconsResolver(),
       ],
-
-      // allow auto import and register components used in markdown
-      // customLoaderMatcher: (id) => id.endsWith('.md'),
-
-      // allow auto load markdown components under `./src/components/`
-      extensions: ['vue'],
-
-      globalComponentsDeclaration: true,
     }),
+    Icons(),
+    // ViteComponents({
+    //
+    //   // auto import icons
+    //   customComponentResolvers: [
+    //     // https://github.com/antfu/vite-plugin-icons
+    //     // ViteIconsResolver({
+    //     //   componentPrefix: '',
+    //     //   enabledCollections: ['carbon'],
+    //     // }),
+    //   ],
+    //
+    //   // allow auto import and register components used in markdown
+    //   // customLoaderMatcher: (id) => id.endsWith('.md'),
+    //
+    //   // allow auto load markdown components under `./src/components/`
+    //   extensions: ['vue'],
+    //
+    //   globalComponentsDeclaration: true,
+    // }),
 
     // https://github.com/antfu/vite-plugin-icons
-    ViteIcons(),
+    // ViteIcons(),
     // https://github.com/antfu/vite-plugin-pwa
     VitePWA({
       includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
