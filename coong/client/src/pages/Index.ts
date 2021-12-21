973c6c1c67c1dfd90faa31c54036092ebf5742f5
Bichi Kim <bichi@live.co.kr>
2021-12-22 04:55:27 +0900

    update client deployment

--- coong/client/src/pages/Index.ts
+++ coong/client/src/pages/Index.ts
@@ -1,6 +1,6 @@
-import {defineComponent, ref} from 'vue'
 import {ZText} from '@namchee/vue-ztext'
 import {QPage} from 'quasar'
+import {defineComponent, ref} from 'vue'
 
 const IndexPage = defineComponent({
   components: {
@@ -15,16 +15,33 @@ const IndexPage = defineComponent({
   },
   template: `
     <q-page v-css="{width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}">
-      <z-text
-        eventRotation="10deg"
-        depth="60px"
-        fade
-        eventDirection="default"
-        event="pointer"
-        v-css="{fontSize: '10em', color: 'lightcoral'}"
-      >
-        Coong 🤩
-      </z-text>
+    <video
+      muted
+      autoplay
+      loop
+      v-css="{
+       minHeight: '100%',
+       maxWidth: '100%',
+       width: '100%',
+       height: '100%',
+       position: 'absolute',
+       objectFit: 'cover',
+       left: '50%',
+       transform: 'translate(-50%, -50%)',
+       top: '50%',
+    }">
+      <source src="/videos/intro.mp4" type="video/mp4">
+    </video>
+    <z-text
+      eventRotation="10deg"
+      depth="60px"
+      fade
+      eventDirection="default"
+      event="pointer"
+      v-css="{fontSize: '10em', color: 'mistyrose'}"
+    >
+      Coong
+    </z-text>
     </q-page>
   `,
 })
