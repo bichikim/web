b4132ccb92e5fd8182539d820735f58189e19597
bichikim <bichi@live.co.kr>
2022-05-12 19:26:19 +0900

    update build

--- coong/client/src/browser-modules/sol-wallet-adapter/index.ts
+++ coong/client/src/browser-modules/sol-wallet-adapter/index.ts
@@ -1,3 +1,3 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-export default process.env.SERVER ? undefined : undefined
+export default process.env.SERVER ? undefined : require('@project-serum/sol-wallet-adapter').default
