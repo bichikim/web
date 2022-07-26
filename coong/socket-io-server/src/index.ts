e70b9f96d574d8b66fa0f9c450ed7f06d9ffda2b
bichikim <bichi@live.co.kr>
2022-07-26 13:40:44 +0900

    update socket io (WIP)

---
+++ coong/socket-io-server/src/index.ts
@@ -0,0 +1,12 @@
+import {Server} from 'socket.io'
+
+const PORT = 3000
+
+const io = new Server(PORT)
+
+io.on('connection', (socket) => {
+  socket.on('ping', (arg) => {
+    console.log(arg)
+    socket.emit('pong', 'pong')
+  })
+})
