f558ffdd93a191b614122659747cef1c96a8af24
bichikim <bichi@live.co.kr>
2022-10-28 13:05:45 +0900

    update

--- packages/utils/src/set-item/index.ts
+++ packages/utils/src/set-item/index.ts
@@ -1,11 +1,6 @@
 import {getItem} from 'src/get-item'
-import {PureObject} from 'src/types'
 
-export const setItem = <T extends PureObject, P = any>(
-  target: T | undefined,
-  path: string[],
-  value: P,
-): void => {
+export const setItem = <T, P = any>(target: T | undefined, path: string[], value: P): void => {
   const _path = [...path]
   const targetName = _path.pop()
   if (!targetName) {
