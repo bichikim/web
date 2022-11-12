4342df5dcca0a0718396a13bcef8482d153d4be5
bichikim <bichi@live.co.kr>
2022-11-13 00:18:31 +0900

    update

--- packages/uni/src/components/glow/UGlow.ts
+++ packages/uni/src/components/glow/UGlow.ts
@@ -1,22 +1,37 @@
-import {styled, css} from 'src/style'
+import {css, styled} from 'src/style'
 
-export const glowCss = css({
-  '&:before': {
-    background: 'inherit',
-    borderRadius: 'inherit',
-    inset: '-5px',
-    filter: 'blur(10px)',
-    content: '',
-    opacity: 0.6,
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    zIndex: -1,
-    transition: 'all 0.5s ease 0s !important'
+export const glowCss = css(
+  {
+    position: 'relative',
+    zIndex: 0,
   },
-  '&:hover:before': {
+  {
+    '&:before': {
+      background: 'inherit',
+      backgroundColor: 'inherit',
+      backgroundSize: 'inherit',
+      borderRadius: 'inherit',
+      bottom: '-5px',
+      content: '',
+      filter: 'blur(10px)',
+      left: '-5px',
+      opacity: 0.6,
+      position: 'absolute',
+      right: '-5px',
+      top: '-5px',
+      transition: '0.5s !important',
+      zIndex: -1,
+    },
+    '&:hover:before': {
+      animation: 'animate 8s linear infinite',
+      bottom: '-5px',
+      filter: 'blur(13px)',
+      left: '-5px',
+      opacity: '1',
+      right: '-5px',
+      top: '-5px',
+    },
+  },
+)
 
-  }
-})
-
-export const UGlow =
+export const UGlow = styled('div', glowCss)
