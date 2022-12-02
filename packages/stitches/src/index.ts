83cdb2539989400f871ac94cdf0b74b5df3c5abe
bichi <bichi@live.co.kr>
2022-12-02 13:21:55 +0900

    update things :)

--- packages/stitches/src/index.ts
+++ packages/stitches/src/index.ts
@@ -6,3 +6,54 @@ export * from './get-directive-store-key'
 export * from './update-class-name'
 export * from '@stitches/core'
 export type {ConfigType, DefaultThemeMap, CreateStitches} from '@stitches/core/types/config'
+export type {Token, ScaleValue} from '@stitches/core/types/theme'
+export type {
+  CSS,
+  Native,
+  CSSProperties,
+  $$PropertyValue,
+  $$ScaleValue,
+  $$ThemeValue,
+} from '@stitches/core/types/css-util'
+export type {
+  Assign,
+  Function,
+  Index,
+  Prefixed,
+  String,
+  Widen,
+  WideObject,
+} from '@stitches/core/types/util'
+export type {
+  CssComponent,
+  TransformProps,
+  StyledComponentProps,
+  StyledComponentType,
+} from '@stitches/core/types/styled-component'
+export type {
+  AtRule,
+  Globals,
+  AtRules,
+  AdvancedPseudos,
+  Pseudos,
+  SimplePseudos,
+  HtmlAttributes,
+  OnlyNumber,
+  OnlyObject,
+  OnlyString,
+  OnlyStringNumeric,
+  Property,
+  Properties,
+  PropertiesFallback,
+  PropertiesHyphenFallback,
+  PropertiesHyphen,
+  StandardLonghandPropertiesHyphenFallback,
+  StandardLonghandProperties,
+  StandardLonghandPropertiesHyphen,
+  StandardLonghandPropertiesFallback,
+  StandardPropertiesFallback,
+  StandardPropertiesHyphenFallback,
+  StandardProperties,
+  StandardPropertiesHyphen,
+  StandardShorthandProperties,
+} from '@stitches/core/types/css'
--- packages/uni/src/index.ts
+++ packages/uni/src/index.ts
@@ -1,2 +1,3 @@
 export * from './components'
 export * from './style'
+export type {Component} from 'vue'
