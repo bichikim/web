de4993bb03576d0c56d7b74762d380b282539148
bichi <bichi@live.co.kr>
2022-12-10 12:32:31 +0900

    update vite

--- coong/coong-client/vite.config.ts
+++ coong/coong-client/vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig(({mode}) => {
       legalComments: 'none',
     },
     optimizeDeps: {
-      exclude: ['vite'],
+      exclude: ['vite', 'vue-demi'],
       include: ['vue', 'vue-router'],
     },
     plugins: [
