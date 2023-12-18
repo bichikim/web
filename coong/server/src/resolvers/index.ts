f63edc37eee85dc589512e3987a8c1f936fd156a
bichi <bichi@live.co.kr>
2023-12-18 14:41:34 +0900

    update server/resolvers

--- coong/server/src/resolvers/index.ts
+++ coong/server/src/resolvers/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import {TestResolver} from './test-resolver/TestResolver'
-import {AuthResolver} from './Auth/AuthResolver'
+import {AuthResolver} from 'src/resolvers/auth/AuthResolver'
 import {NonEmptyArray} from 'type-graphql/build/typings'
 
 export interface Resolver {
