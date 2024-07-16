e41e5b560c7710740501a27c354fb511e81c94a0
bichi <bichi@live.co.kr>
2024-07-16 11:22:23 +0900

    update

--- packages/kata/src/10828.ts
+++ packages/kata/src/10828.ts
@@ -1,7 +1,7 @@
 export const push =
   (value: any) =>
-  ([stack, print]: [any[], string[]]) => [[value, ...stack], print]
-export const pop = ([stack, print]) => {
+  ([stack, print]: [any[], string[]]): [any[], string[]] => [[value, ...stack], print]
+export const pop = ([stack, print]: [any[], string[]]): [any[], string[]] => {
   const [value, ...rest] = stack
 
   if (value === undefined) {
@@ -11,15 +11,15 @@ export const pop = ([stack, print]) => {
   return [rest, [...print, `${value}`]]
 }
 
-export const size = ([stack, print]: [any[], string[]]) => {
+export const size = ([stack, print]: [any[], string[]]): [any[], string[]] => {
   return [stack, [...print, `${stack.length}`]]
 }
 
-export const empty = ([stack, print]: [any[], string[]]) => {
+export const empty = ([stack, print]: [any[], string[]]): [any[], string[]] => {
   return [stack, [...print, `${stack.length === 0 ? 1 : 0}`]]
 }
 
-export const top = ([stack, print]) => {
+export const top = ([stack, print]): [any[], string[]] => {
   const [value] = stack
 
   if (value === undefined) {
@@ -29,8 +29,8 @@ export const top = ([stack, print]) => {
   return [stack, [...print, `${value}`]]
 }
 
-export const pipe = (executors: ((arg: any) => any)[]) => {
-  return <T>(arg: T): T => {
+export const pipe = <T>(executors: ((arg: T) => T)[]) => {
+  return (arg: T): T => {
     let result = arg
     for (const executor of executors) {
       result = executor(result)
