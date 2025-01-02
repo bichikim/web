4201d072b51e9b1ade9d43916738de116113b9bf
bichi <bichi@live.co.kr>
2025-01-02 18:23:19 +0900

    update

--- packages/utils/src/is-scroll-able/index.ts
+++ packages/utils/src/is-scroll-able/index.ts
@@ -3,8 +3,6 @@ const scrollRegex = /(auto|scroll)/u
 
 export const isScrollAble = (element: Element) => {
   return scrollRegex.test(
-    getStyle(element, 'overflow') +
-      getStyle(element, 'overflowX') +
-      getStyle(element, 'overflowY'),
+    `${getStyle(element, 'overflow')}${getStyle(element, 'overflowX')}${getStyle(element, 'overflowY')}`,
   )
 }
