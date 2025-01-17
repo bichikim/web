120edd086d9ea3d85e2af41647f101b97ad0d29b
bichi <bichi@live.co.kr>
2025-01-17 11:36:43 +0900

    update

--- apps/bplan-client/src/routes/(main-layout)/preset/[id].tsx
+++ apps/bplan-client/src/routes/(main-layout)/preset/[id].tsx
@@ -1,6 +1,7 @@
 import {createResource} from 'solid-js'
 import {MusicInfo} from 'src/components/midi-player'
 import {default as HomePage} from 'src/routes/(main-layout)/(home)'
+import {useParams} from '@solidjs/router'
 
 const getSelfUrl = () => {
   return import.meta.env.VITE_API_URL ?? 'http://localhost:3000'
@@ -11,7 +12,7 @@ interface Data {
   title: string
 }
 const getPreset = async (id?: string): Promise<Data | undefined> => {
-  // 'use server'
+  'use server'
 
   if (!id) {
     return
@@ -27,11 +28,11 @@ const getPreset = async (id?: string): Promise<Data | undefined> => {
 }
 
 export default function PresetPage() {
-  // const params = useParams()
+  const params = useParams()
   // solidjs/router query send empty page time to time
   // do not use createAsync because it create a template and it does not work
   // createResource also make the template and it does not work
-  const [preset] = createResource(() => getPreset())
+  const [preset] = createResource(() => getPreset(params.id))
   // const preset = () => ({musics: [], title: 'Unknown'})
 
   return <HomePage presetTitle={preset()?.title} initMusics={preset()?.musics} />
