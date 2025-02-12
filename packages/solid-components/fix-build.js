6a247b8028cd80b802f6fb5f48530f8aa53992a3
bichi <bichi@live.co.kr>
2025-02-12 23:59:25 +0900

    chore(solid-components): update build configuration and dependencies
    - support alias src

---
+++ packages/solid-components/fix-build.js
@@ -0,0 +1,13 @@
+import {readFileSync, writeFileSync} from 'node:fs'
+
+const modulePath = await import.meta.resolve('rollup-preset-solid')
+
+const code = readFileSync(modulePath.replace('file://', ''), 'utf8')
+
+const removedNodeNextPluginCode = code.replace(
+  // eslint-disable-next-line require-unicode-regexp
+  /{\n\s*name: "fix-import-extensions",\n(.|\n)*emit\(\);\n\s*}\n\s*},/,
+  '',
+)
+
+writeFileSync(modulePath.replace('file://', ''), removedNodeNextPluginCode)
