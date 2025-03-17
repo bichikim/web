c62a337ba4b843a1b893e542632793bfbd85e8a0
bichi <bichi@live.co.kr>
2025-03-17 11:05:29 +0900

    refactor remove unused get-alias-path

--- packages/vite-plugin-monorepo-alias/src/__tests__/get-alias-path.spec.ts
+++
@@ -1,9 +0,0 @@
-import {getAliasPath} from '../get-alias-path'
-import {describe, expect, it} from 'vitest'
-const workspacePath = '^/Users/app/packages/[-/._a-zA-Z0-9]*/'
-
-describe('getAliasPath', () => {
-  it('should return alias with string', () => {
-    expect(getAliasPath(workspacePath, 'src'))
-  })
-})
