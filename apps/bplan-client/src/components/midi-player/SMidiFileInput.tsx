1fcc673e8adec9d7b5b89a2feb81d4760dc9a0dc
bichi <bichi@live.co.kr>
2025-03-21 17:41:21 +0900

    implement useMidiFileInput hook for handling MIDI file input, enhancing separation of concerns and reusability in SMidiFileInput component

--- apps/bplan-client/src/components/midi-player/SMidiFileInput.tsx
+++ apps/bplan-client/src/components/midi-player/SMidiFileInput.tsx
@@ -1,18 +1,7 @@
-import {
-  createMemo,
-  createSignal,
-  createUniqueId,
-  JSX,
-  onCleanup,
-  splitProps,
-} from 'solid-js'
+import {createSignal, createUniqueId, JSX, splitProps} from 'solid-js'
 import {cva} from 'class-variance-authority'
-import type {Midi} from '@tonejs/midi'
 import {MusicInfo} from 'src/components/midi-player/SFileItem'
-import {loadMidi} from 'src/utils/read-midi'
-import {SampleStart} from './types'
-import {useEvent} from '@winter-love/solid-use'
-import {getWindow} from '@winter-love/utils'
+import {useMidiFileInput} from './midi-file-input'
 
 export interface HMidiFileInputProps
   extends Omit<
@@ -57,138 +46,24 @@ export const SMidiFileInput = (props: HMidiFileInputProps) => {
     'onClick',
   ])
   const [inputElement, setInputElement] = createSignal<HTMLInputElement | null>(null)
-  const [isFocused, setIsFocused] = createSignal(false)
-  const [isTouchStart, setIsTouchStart] = createSignal(false)
-  const [isDragOver, setIsDragOver] = createSignal(false)
-  let isCleanup = false
+
   // let isTouchStart = false
 
-  const handleInputFiles = async (files: FileList | null) => {
-    if (!files || files.length === 0) {
-      return
-    }
-
-    const promiseList: Promise<{midi: Midi; name: string}>[] = []
-
-    // FileList 는 이터레이블이 아닙니다
-    // eslint-disable-next-line unicorn/no-for-loop
-    for (let index = 0; index < files.length; index += 1) {
-      const file = files[index]
-
-      promiseList.push(loadMidi(file))
-    }
-
-    const midis = await Promise.all(promiseList)
-
-    if (isCleanup) {
-      return
-    }
-
-    const samples: MusicInfo[] = midis
-      .map((midiFile, index): MusicInfo | null => {
-        const {name, midi} = midiFile
-        const {header} = midi
-
-        const midiData: SampleStart[][] = midi.tracks
-          .map((track) => {
-            const {notes, instrument: {family} = {}} = track
-
-            if (!notes || family !== 'piano') {
-              return null
-            }
-
-            return notes.map((track): SampleStart => {
-              return {
-                duration: track.duration,
-                note: track.name,
-                time: track.time,
-                velocity: track.velocity,
-              }
-            })
-          })
-          .filter(Boolean) as SampleStart[][]
-        let totalDuration = 0
-
-        for (const track of midiData) {
-          const lastNote = track.at(-1)
-
-          if (lastNote) {
-            const trackTotal = (lastNote.time ?? 0) + (lastNote.duration ?? 0)
-
-            if (trackTotal > totalDuration) {
-              totalDuration = trackTotal
-            }
-          }
-        }
-
-        const [singleTrack] = midi.tracks
-
-        if (!singleTrack) {
-          return null
-        }
-
-        return {
-          ext: 'midi',
-          header,
-          id: `${name}-${index}`,
-          midi: midiData.filter(Boolean) as SampleStart[][],
-          name: name.replace(/\.mid$/u, ''),
-          totalDuration,
-        }
-      })
-      .filter(Boolean) as MusicInfo[]
-
-    innerProps.onAdd?.(samples)
-  }
-
-  const globalTarget = createMemo(() => (isTouchStart() ? getWindow() : null))
-
-  useEvent(globalTarget, 'touchend', () => {
-    if (isTouchStart()) {
-      inputElement()?.click()
-    }
-
-    setIsTouchStart(false)
+  const {
+    isDragOver,
+    isFocused,
+    handleInputFiles,
+    handleDragOver,
+    handleDragLeave,
+    handleDrop,
+    handleFocus,
+    handleBlur,
+    handleInputClick,
+    handleTouchStart,
+  } = useMidiFileInput(inputElement, {
+    onAdd: props.onAdd,
+    onClick: props.onClick,
   })
-
-  onCleanup(() => {
-    isCleanup = true
-  })
-
-  const handleTouchStart = () => {
-    setIsTouchStart(true)
-  }
-
-  const handleInputClick: (event: any) => void = (event: PointerEvent) => {
-    props.onClick?.(event)
-
-    // 터치 끝날 시 클릭 방지 터치가 시작된후 바로 그 시작된 엘리먼트 자리에 다른 엘리먼트가 있을 경우 클릭 방지
-    if (!isTouchStart() && event.pointerType === 'touch') {
-      event.preventDefault()
-    }
-
-    setIsTouchStart(false)
-  }
-
-  const handleDragOver = () => {
-    setIsDragOver(true)
-  }
-
-  const handleDragLeave = () => {
-    setIsDragOver(false)
-  }
-
-  const handleDrop = () => {
-    setIsDragOver(false)
-  }
-
-  const handleFocus = () => {
-    setIsFocused(true)
-  }
-
-  const handleBlur = () => {
-    setIsFocused(false)
-  }
 
   return (
     <div
@@ -198,12 +73,7 @@ export const SMidiFileInput = (props: HMidiFileInputProps) => {
         isFocused: isFocused(),
       })}
     >
-      <label
-        class={labelStyle}
-        tabIndex="-1"
-        onDragOver={handleDragOver}
-        aria-controls={id}
-      >
+      <label class={labelStyle} tabIndex="-1" onDragOver={handleDragOver} for={id}>
         <span class=":uno: text-6 md:pt-.5 sm:inline-block hidden truncate flex-shrink-1">
           Click or Drop{' '}
         </span>
