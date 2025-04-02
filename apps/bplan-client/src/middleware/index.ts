bb2ae472bc5a1cb73d4bb882688edee2e7b104c6
bichi <bichi@live.co.kr>
2025-04-02 11:25:52 +0900

    refactor middleware session handling to use getSession directly from @auth/solid-start; add JSDoc comment for clarity on request processing

--- apps/bplan-client/src/middleware/index.ts
+++ apps/bplan-client/src/middleware/index.ts
@@ -1,10 +1,14 @@
 import {createMiddleware} from '@solidjs/start/middleware'
-import {getSession as _getSession} from '@auth/solid-start'
+import {getSession} from '@auth/solid-start'
 import {authOptions} from 'src/server/auth/config'
 
-// uses via vite.config.mts -> middleware: 'src/middleware/index.ts'
+/**
+ * set session to event.locals on request
+ * uses via vite.config.mts -> middleware: 'src/middleware/index.ts'
+ */
 export default createMiddleware({
   onRequest: (event) => {
-    event.locals.session = _getSession(event.request, authOptions)
+    // insert session to locals from auth/solid-start
+    event.locals.session = getSession(event.request, authOptions)
   },
 })
