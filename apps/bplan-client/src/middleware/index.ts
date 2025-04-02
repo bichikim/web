8b5fbfe11af5fa0e49cf65d31fd844ca4adc9848
bichi <bichi@live.co.kr>
2025-04-02 11:01:08 +0900

    add comment to middleware index.ts to clarify its usage in vite.config.mts

--- apps/bplan-client/src/middleware/index.ts
+++ apps/bplan-client/src/middleware/index.ts
@@ -2,6 +2,7 @@ import {createMiddleware} from '@solidjs/start/middleware'
 import {getSession as _getSession} from '@auth/solid-start'
 import {authOptions} from 'src/server/auth/config'
 
+// uses via vite.config.mts -> middleware: 'src/middleware/index.ts'
 export default createMiddleware({
   onRequest: (event) => {
     event.locals.session = _getSession(event.request, authOptions)
