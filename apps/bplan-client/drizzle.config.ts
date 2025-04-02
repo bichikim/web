a033212e59eda3a25a96bae2792be17a634ce63f
bichi <bichi@live.co.kr>
2025-04-02 11:29:20 +0900

    add JSDoc comments to drizzle.config.ts for table naming conventions and dialect information

--- apps/bplan-client/drizzle.config.ts
+++ apps/bplan-client/drizzle.config.ts
@@ -2,10 +2,17 @@ import 'dotenv/config'
 import {defineConfig} from 'drizzle-kit'
 
 export default defineConfig({
+  /**
+   * use snake_case for table name whatever key case is
+   */
   casing: 'snake_case',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
+  /**
+   * postgres is Vercel Neno
+   * @see https://vercel.com/bichis-projects/web/stores
+   */
   dialect: 'postgresql',
   out: './drizzle',
   schema: './src/server/db/schema',
