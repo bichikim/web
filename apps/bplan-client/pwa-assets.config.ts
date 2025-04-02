3a105089b80344dab8d27b5b9f9e0724967337de
bichi <bichi@live.co.kr>
2025-04-02 11:26:18 +0900

    add JSDoc comment to pwa-assets.config.ts for PWA assets generation command

--- apps/bplan-client/pwa-assets.config.ts
+++ apps/bplan-client/pwa-assets.config.ts
@@ -1,5 +1,9 @@
 import {defineConfig} from '@vite-pwa/assets-generator/config'
 
+/**
+ * for pwa-assets-generator command
+ * run `pnpm generate-pwa-assets`
+ */
 export default defineConfig({
   images: ['public/favicon.svg'],
   preset: 'minimal-2023',
