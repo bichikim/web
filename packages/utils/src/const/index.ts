3245e2244ed00e0448acd9c62b221cb7a05dca83
bichi <bichi@live.co.kr>
2025-04-17 22:12:52 +0900

    add constants for data size units in utils: ONE_KB and ONE_MB

--- packages/utils/src/const/index.ts
+++ packages/utils/src/const/index.ts
@@ -1,6 +1,11 @@
+export const TEN = 10
 export const HUNDRED = 100
+
 export const HALF = 2
 export const THREE_TIMES = 3
+
+export const ONE_KB = 1024
+export const ONE_MB = ONE_KB * ONE_KB
 
 export const ZERO = 0
 export const ONE = 1
