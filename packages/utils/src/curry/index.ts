7eed25a77f307c779a70d2007469330871a3f5e3
bichi <bichi@live.co.kr>
2025-10-31 20:44:36 +0900

    refactor utils/curry type

--- packages/utils/src/curry/index.ts
+++ packages/utils/src/curry/index.ts
@@ -1,4 +1,7 @@
-/* eslint-disable no-magic-numbers */
+interface CurriedFunction0<R> {
+  (): R
+}
+
 interface CurriedFunction1<T1, R> {
   (t2: T1): R
 }
@@ -107,68 +110,106 @@ interface CurriedFunction4And4<T1, T2, T3, T4, R> {
   (t1?: T1, t2?: T2, t3?: T3, t4?: T4): R
 }
 
-export function curry<T1, R>(f: (t1?: T1) => R, length?: number, args?: unknown[]): CurriedFunction1And1<T1, R>
-export function curry<T1, R>(f: (t1: T1) => R, length?: number, args?: unknown[]): CurriedFunction1<T1, R>
-export function curry<T1, T2, R>(
-  f: (t1?: T1, t2?: T2) => R,
+type Length<T extends readonly any[]> = T['length']
+
+export function curry<T extends readonly any[], R>(
+  f: (...args: T) => R,
   length?: number,
   args?: unknown[],
-): CurriedFunction2And2<T1, T2, R>
-export function curry<T1, T2, R>(
-  f: (t1: T1, t2?: T2) => R,
-  length?: number,
-  args?: unknown[],
-): CurriedFunction2And1<T1, T2, R>
-export function curry<T1, T2, R>(
-  f: (t1: T1, t2: T2) => R,
-  length?: number,
-  args?: unknown[],
-): CurriedFunction2<T1, T2, R>
-export function curry<T1, T2, T3, R>(
-  f: (t1?: T1, t2?: T2, t3?: T3) => R,
-  length?: number,
-  args?: unknown[],
-): CurriedFunction3And3<T1, T2, T3, R>
-export function curry<T1, T2, T3, R>(
-  f: (t1: T1, t2?: T2, t3?: T3) => R,
-  length?: number,
-  args?: unknown[],
-): CurriedFunction3And2<T1, T2, T3, R>
-export function curry<T1, T2, T3, R>(
-  f: (t1: T1, t2: T2, t3?: T3) => R,
-  length?: number,
-  args?: unknown[],
-): CurriedFunction3And1<T1, T2, T3, R>
-export function curry<T1, T2, T3, R>(
-  f: (t1: T1, t2: T2, t3: T3) => R,
-  length?: number,
-  args?: unknown[],
-): CurriedFunction3<T1, T2, T3, R>
-export function curry<T1, T2, T3, T4, R>(
-  f: (t1?: T1, t2?: T2, t3?: T3, t4?: T4) => R,
-  length?: number,
-  args?: unknown[],
-): CurriedFunction4And4<T1, T2, T3, T4, R>
-export function curry<T1, T2, T3, T4, R>(
-  f: (t1: T1, t2?: T2, t3?: T3, t4?: T4) => R,
-  length?: number,
-  args?: unknown[],
-): CurriedFunction4And3<T1, T2, T3, T4, R>
-export function curry<T1, T2, T3, T4, R>(
-  f: (t1: T1, t2: T2, t3?: T3, t4?: T4) => R,
-  length?: number,
-  args?: unknown[],
-): CurriedFunction4And2<T1, T2, T3, T4, R>
-export function curry<T1, T2, T3, T4, R>(
-  f: (t1: T1, t2: T2, t3: T3, t4?: T4) => R,
-  length?: number,
-  args?: unknown[],
-): CurriedFunction4And1<T1, T2, T3, T4, R>
-export function curry<T1, T2, T3, T4, R>(
-  f: (t1: T1, t2: T2, t3: T3, t4: T4) => R,
-  length?: number,
-  args?: unknown[],
-): CurriedFunction4<T1, T2, T3, T4, R>
+): Length<T> extends 0
+  ? CurriedFunction0<R>
+  : Length<T> extends 1
+    ? CurriedFunction1<T[0], R>
+    : Length<T> extends 0 | 1
+      ? CurriedFunction1And1<T[0], R>
+      : Length<T> extends 2
+        ? CurriedFunction2<T[0], T[1], R>
+        : Length<T> extends 2 | 1
+          ? CurriedFunction2And1<T[0], T[1], R>
+          : Length<T> extends 2 | 1 | 0
+            ? CurriedFunction2And2<T[0], T[1], R>
+            : Length<T> extends 3
+              ? CurriedFunction3<T[0], T[1], T[2], R>
+              : Length<T> extends 3 | 2
+                ? CurriedFunction3And1<T[0], T[1], T[2], R>
+                : Length<T> extends 3 | 2 | 1
+                  ? CurriedFunction3And2<T[0], T[1], T[2], R>
+                  : Length<T> extends 3 | 2 | 1 | 0
+                    ? CurriedFunction3And3<T[0], T[1], T[2], R>
+                    : Length<T> extends 4
+                      ? CurriedFunction4<T[0], T[1], T[2], T[3], R>
+                      : Length<T> extends 4 | 3
+                        ? CurriedFunction4And1<T[0], T[1], T[2], T[3], R>
+                        : Length<T> extends 4 | 3 | 2
+                          ? CurriedFunction4And2<T[0], T[1], T[2], T[3], R>
+                          : Length<T> extends 4 | 3 | 2 | 1
+                            ? CurriedFunction4And3<T[0], T[1], T[2], T[3], R>
+                            : Length<T> extends 4 | 3 | 2 | 1 | 0
+                              ? CurriedFunction4And4<T[0], T[1], T[2], T[3], R>
+                              : never
+
+// export function curry<T1, R>(f: (t1?: T1) => R, length?: number, args?: unknown[]): CurriedFunction1And1<T1, R>
+// export function curry<T1, R>(f: (t1: T1) => R, length?: number, args?: unknown[]): CurriedFunction1<T1, R>
+// export function curry<T1, T2, R>(
+//   f: (t1?: T1, t2?: T2) => R,
+//   length?: number,
+//   args?: unknown[],
+// ): CurriedFunction2And2<T1, T2, R>
+// export function curry<T1, T2, R>(
+//   f: (t1: T1, t2?: T2) => R,
+//   length?: number,
+//   args?: unknown[],
+// ): CurriedFunction2And1<T1, T2, R>
+// export function curry<T1, T2, R>(
+//   f: (t1: T1, t2: T2) => R,
+//   length?: number,
+//   args?: unknown[],
+// ): CurriedFunction2<T1, T2, R>
+// export function curry<T1, T2, T3, R>(
+//   f: (t1?: T1, t2?: T2, t3?: T3) => R,
+//   length?: number,
+//   args?: unknown[],
+// ): CurriedFunction3And3<T1, T2, T3, R>
+// export function curry<T1, T2, T3, R>(
+//   f: (t1: T1, t2?: T2, t3?: T3) => R,
+//   length?: number,
+//   args?: unknown[],
+// ): CurriedFunction3And2<T1, T2, T3, R>
+// export function curry<T1, T2, T3, R>(
+//   f: (t1: T1, t2: T2, t3?: T3) => R,
+//   length?: number,
+//   args?: unknown[],
+// ): CurriedFunction3And1<T1, T2, T3, R>
+// export function curry<T1, T2, T3, R>(
+//   f: (t1: T1, t2: T2, t3: T3) => R,
+//   length?: number,
+//   args?: unknown[],
+// ): CurriedFunction3<T1, T2, T3, R>
+// export function curry<T1, T2, T3, T4, R>(
+//   f: (t1?: T1, t2?: T2, t3?: T3, t4?: T4) => R,
+//   length?: number,
+//   args?: unknown[],
+// ): CurriedFunction4And4<T1, T2, T3, T4, R>
+// export function curry<T1, T2, T3, T4, R>(
+//   f: (t1: T1, t2?: T2, t3?: T3, t4?: T4) => R,
+//   length?: number,
+//   args?: unknown[],
+// ): CurriedFunction4And3<T1, T2, T3, T4, R>
+// export function curry<T1, T2, T3, T4, R>(
+//   f: (t1: T1, t2: T2, t3?: T3, t4?: T4) => R,
+//   length?: number,
+//   args?: unknown[],
+// ): CurriedFunction4And2<T1, T2, T3, T4, R>
+// export function curry<T1, T2, T3, T4, R>(
+//   f: (t1: T1, t2: T2, t3: T3, t4?: T4) => R,
+//   length?: number,
+//   args?: unknown[],
+// ): CurriedFunction4And1<T1, T2, T3, T4, R>
+// export function curry<T1, T2, T3, T4, R>(
+//   f: (t1: T1, t2: T2, t3: T3, t4: T4) => R,
+//   length?: number,
+//   args?: unknown[],
+// ): CurriedFunction4<T1, T2, T3, T4, R>
 
 export function curry(target: (...args: unknown[]) => unknown, length: number = target.length, args: unknown[] = []) {
   const _args: unknown[] = args
