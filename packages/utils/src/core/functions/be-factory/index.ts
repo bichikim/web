068c2cd20ba0b4ce966d45ae0e6baa46f3eeedda
kimbichi <bichi@live.co.kr>
2026-08-02 16:09:11 +0900

    refactor - remove beFactory and enforce PR type checks

--- packages/utils/src/core/functions/be-factory/index.ts
+++ packages/utils/src/core/functions/be-factory/index.ts
@@ -1,14 +1,9 @@
-import {Drop, Shift} from 'src/core/types/shared'
-
-export const flipArgsFactory = <T extends (...args: any) => any>(func: T) => {
-  return (...args: Drop<Parameters<T>>) => {
-    return (value: Shift<Parameters<T>>) => {
+export const flipArgsFactory = <First, Rest extends unknown[], Result>(
+  func: (value: First, ...args: Rest) => Result,
+) => {
+  return (...args: Rest) => {
+    return (value: First): Result => {
       return func(value, ...args)
     }
   }
 }
-
-/**
- * @deprecated Use `flipArgsFactory` instead
- */
-export const beFactory = flipArgsFactory
